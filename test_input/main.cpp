230f2d9d8e3214a1c10db4852ae8a9abda2ec688
hira <hira@rvf-rc45.net>
2017-02-01 13:39:28 +0900

    update float

--- test_input/main.cpp
+++ test_input/main.cpp
@@ -16,17 +16,18 @@ int main(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
-
 	using namespace utils;
+#if 0
+	int a = 0;
+	uint32_t b = 0;
 
+	auto n = (input("%d[, ]%x", "-99 100") % a % b).num();
+	std::cout << "Num: " << n << std::endl;
+	std::cout << "1ST: " << a << ", 2ND: " << b << std::endl;
+#endif
+	float a;
 
-	int a;
-	uint32_t b;
-
-	input("%d[, ]%x", "-99 100") % a % b;
-
-	std::cout << a << ", " << b << std::endl;
-
-	auto n = input::get_conversion();
-	std::cout << "N: " << n << std::endl;
+	auto n = (input("%f", "101.945") % a).num();
+	std::cout << "Num: " << n << std::endl;
+	std::cout << "Ans: " << a << std::endl;
 }
