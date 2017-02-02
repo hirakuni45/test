46257379924ac7bbbc4ec4da89ccb41b88690609
hira <hira@rvf-rc45.net>
2017-02-02 09:03:04 +0900

    update test

--- test_input/main.cpp
+++ test_input/main.cpp
@@ -28,10 +28,12 @@ int main(int argc, char* argv[])
 	float a;
 	int b;
 	uint32_t c;
+	double d;
 
-	auto n = (input("%f,%d,%x", "101.945,-76,7ff") % a % b % c).num();
+	auto n = (input("%f,%d,%x,%f", "101.945,-76,7ff,1.567") % a % b % c % d).num();
 	std::cout << "Num: " << n << std::endl;
 	std::cout << "1ST: " << a << std::endl;
 	std::cout << "2ND: " << b << std::endl;
 	std::cout << "3RD: " << c << std::endl;
+	std::cout << "4TH: " << d << std::endl;
 }
