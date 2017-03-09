e4cf29ec9f8bac6da625f6d168c3bdd2c3217a3c
hira <hira@rvf-rc45.net>
2017-03-09 14:57:24 +0900

    update

--- calc_inductor/main.cpp
+++ calc_inductor/main.cpp
@@ -10,27 +10,34 @@ namespace {
 	{
 		return ((vin - vout) * vout) / (vin * fsw * r * iout); 
 	}
+
+	double ipeak_(double iout, double r) {
+		return iout + (r * iout) / 2.0;
+	}
 }
 
 int main(int argc, char* argv[])
 {
+#if 1
 	double vinp = 100.0 * 1.41421356;
-	double vout = 140.0;
-	double iout = 5.0;
+	double vout = 110.0;
+	double iout = 3.0;
 	double r = 0.3;
 	double fsw = 96e6 / 512.0;
 
 	{
 		auto a = calc_indu_(vinp, vout, iout, r, fsw);
+		auto b = ipeak_(iout, r);
 		utils::format("Vinp: %6.3f, ") % vinp;
 		utils::format("Vout: %6.3f, ") % vout;
 		utils::format("Iout: %6.3f, ") % iout;
+		utils::format("Peak: %6.3f, ") % b;
 		utils::format("%6.3f [uH]\n") % (a * 1e6);
 	}
 
 	{
-		vout = 70.0;
-		iout = 2.5;
+		vout = 55.0;
+		iout = 1.5;
 		auto a = calc_indu_(vinp, vout, iout, r, fsw);
 		utils::format("Vinp: %6.3f, ") % vinp;
 		utils::format("Vout: %6.3f, ") % vout;
@@ -41,10 +48,42 @@ int main(int argc, char* argv[])
 	{
 		vout = 5.0;
 		iout = 0.1;
+		r = 0.5;
 		auto a = calc_indu_(vinp, vout, iout, r, fsw);
 		utils::format("Vinp: %6.3f, ") % vinp;
 		utils::format("Vout: %6.3f, ") % vout;
 		utils::format("Iout: %6.3f, ") % iout;
 		utils::format("%6.3f [uH]\n") % (a * 1e6);
 	}
+#endif
+
+#if 0
+	double vinp = 20.0;
+	double vout = 18.0;
+	double iout = 10.0;
+	double r = 0.3;
+	double fsw = 80e6 / 512.0;
+
+	{
+		auto a = calc_indu_(vinp, vout, iout, r, fsw);
+		auto b = ipeak_(iout, r);
+		utils::format("Vinp: %6.3f, ") % vinp;
+		utils::format("Vout: %6.3f, ") % vout;
+		utils::format("Iout: %6.3f, ") % iout;
+		utils::format("Peak: %6.3f, ") % b;
+		utils::format("%6.3f [uH]\n") % (a * 1e6);
+	}
+
+	{
+		vout = 0.5;
+		iout = 0.1;
+		auto a = calc_indu_(vinp, vout, iout, r, fsw);
+		auto b = ipeak_(iout, r);
+		utils::format("Vinp: %6.3f, ") % vinp;
+		utils::format("Vout: %6.3f, ") % vout;
+		utils::format("Iout: %6.3f, ") % iout;
+		utils::format("Peak: %6.3f, ") % b;
+		utils::format("%6.3f [uH]\n") % (a * 1e6);
+	}
+#endif
 }
