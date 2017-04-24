cb81b35f228e16682ae8fd770d30ec936da5a2ae
hira <hira@rvf-rc45.net>
2017-04-24 16:34:38 +0900

    new project

---
+++ socket/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+namespace {
+
+}
+
+int main(int argc, char* argv[]);
+
+int main(int argc, char* argv[])
+{
+    // サーバーソケット作成
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    // struct sockaddr_in 作成
+    struct sockaddr_in sa = {0};
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(3000);
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // バインド
+    if (bind(sock, (struct sockaddr*) &sa, sizeof(struct sockaddr_in)) == -1)
+    {
+        perror("bind");
+	    close(sock);
+    	return 1;
+    }
+
+    // リッスン
+    if (listen(sock, 128) == -1)
+    {
+        perror("listen");
+	    close(sock);
+    	return 1;
+    }
+
+	// クライアントの接続を待つ
+	int fd;
+	fd = accept(sock, NULL, NULL);
+	if (fd == -1)
+	{
+		perror("accept");
+	    close(sock);
+    	return 1;
+	}
+
+	std::string line;
+	bool term = false;
+    while(!term) {
+        // 受信
+        char buffer[4096];
+        int recv_size = read(fd, buffer, sizeof(buffer) - 1);
+        if(recv_size == -1) {
+            perror("read");
+            close(fd);
+		    close(sock);
+			return 1;
+        }
+
+		// 内容を解析して表示
+		for(int i = 0; i < recv_size; ++i) {
+			char ch = buffer[i];
+			if(ch == '\r') continue;
+			if(ch == '\n') {
+				std::cout << line << std::endl;
+				if(line == "end") term = true;
+				line.clear();
+			} else {
+				line += ch;
+			}
+		}
+    }
+
+	// 接続のクローズ
+	if(close(fd) == -1)
+	{
+		perror("close");
+	    close(sock);
+		return 1;
+	}
+
+	close(sock);
+}
