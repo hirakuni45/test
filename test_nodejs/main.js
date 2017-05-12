2cfafe5dc3edd21bbd0a372746eecf82e3282256
hira <hira@rvf-rc45.net>
2017-05-12 21:04:21 +0900

    http post server

---
+++ test_nodejs/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var http = require("http"),
+    url  = require("url"),
+    server;
+
+server = http.createServer(function (request, response) {
+  request.on("end", function() {
+    var urlObj;
+
+    switch (request.method) {
+      case "POST":
+        urlObj = url.parse(request.url, true);
+
+        switch (urlObj.pathname) {
+          case "/api/":
+            console.log(urlObj.query.val);
+            response.end();
+            break;
+        }
+        break;
+    }
+  }).resume();
+}).listen(3000);
