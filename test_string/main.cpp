d153a9f6404a7be8380a8691347207e3543fc1c0
hira <hira@rvf-rc45.net>
2017-06-12 04:22:00 +0900

    new project

---
+++ test_string/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "../test_format/format.hpp"
+#include "fixed_string.hpp"
+
+int main(int argc, char* argv[]);
+
+int main(int argc, char* argv[])
+{
+	using namespace utils;
+
+
+}
