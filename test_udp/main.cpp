bf748313f93da61b837f9c1939b7ab7f7028ca53
hira <hira@rvf-rc45.net>
2017-07-04 16:58:08 +0900

    update test manage

--- test_udp/main.cpp
+++ test_udp/main.cpp
@@ -40,13 +40,14 @@ int main(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
+	srand(static_cast<unsigned>(time(NULL)));
+
 	// ソケット作成
 	int sock = socket(PF_INET, SOCK_DGRAM, 0);
 	if(sock < 0) {
 		perror("UDP socket");
 		return 1;
 	}
-	utils::format("UDP Open: %d\n") % sock;
 
 	// struct sockaddr_in 作成
 	struct sockaddr_in cl = { 0 };
@@ -55,27 +56,30 @@ int main(int argc, char* argv[])
 	cl.sin_port = htons(3000);
 	// 接続先のアドレスを指定（指定しない事も可能）
 	cl.sin_addr.s_addr = inet_addr("192.168.3.2");
+	utils::format("UDP Open(%d): '%s' (%d)\n")
+		% sock % get_ip_str(cl.sin_addr.s_addr) % htons(cl.sin_port);
 
-	// 送信
-	utils::format("UDP Send: %s (%d)\n")
-		% get_ip_str(cl.sin_addr.s_addr)
-		% static_cast<int>(htons(cl.sin_port));
+	// ＵＤＰテスト側と同じにする
+	int loop_num = 4;
 
-	static const char* msg = { "AbcdefgH" };
-	int len = std::strlen(msg);
-	sendto(sock, msg, len, 0, (struct sockaddr *)&cl, sizeof(cl));
-	utils::format("UDP Client sendto: '%s', %d bytes\n") % msg % len;
+	for(int loop = 0; loop < loop_num; ++loop) {
+		// 送信
+		uint32_t send_len = (rand() & 15) + 4;
+		char send_tmp[32];
+		for(uint32_t i = 0; i < send_len; ++i) {
+			send_tmp[i] = 32 + (rand() & 63);
+		}
+		sendto(sock, send_tmp, send_len, 0, (struct sockaddr *)&cl, sizeof(cl));
+		utils::format("UDP Client sendto: '%s', %d bytes\n") % send_tmp % send_len;
 
-	// 受信
-	utils::format("UDP Recv: %s (%d)\n")
-		% get_ip_str(cl.sin_addr.s_addr)
-		% static_cast<int>(htons(cl.sin_port));
-	char tmp[64];
-	socklen_t addrlen = sizeof(struct sockaddr);
-	int ret = recvfrom(sock, tmp, sizeof(tmp), 0, (struct sockaddr *)&cl, &addrlen);
-	if(ret > 0) {
-		tmp[ret] = 0;
-		utils::format("UDP Client recvfrom: '%s', %d bytes\n") % tmp % ret;
+		// 受信
+		char recv_tmp[32];
+		socklen_t addrlen = sizeof(struct sockaddr);
+		int recv_len = recvfrom(sock, recv_tmp, sizeof(recv_tmp), 0, (struct sockaddr *)&cl, &addrlen);
+		if(recv_len >= 0) {
+			recv_tmp[recv_len] = 0;
+			utils::format("UDP Client recvfrom: '%s', %d bytes\n") % recv_tmp % recv_len;
+		}
 	}
 
 	// 接続のクローズ
