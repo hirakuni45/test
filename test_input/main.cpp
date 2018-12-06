e34d0c7b80f6ec1849ba054057665d30bf888ba7
hira <hira@rvf-rc45.net>
2018-12-06 16:57:21 +0900

    update: cleanup

--- test_input/main.cpp
+++ test_input/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 	using namespace utils;
 
 #if 0
-	{
+	{ // test 1
 		float a;
 		int n = std::sscanf("    100.54  ", "%f", &a);
 		std::cout << "sscanf N: " << n << ", " << a << std::endl;
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
 #endif
 
 #if 1
-	{
+	{  // test 2
 		float a;
 		if((input("%f", "2.5e3") % a).status()) {
 			std::cout << a << std::endl;
@@ -31,27 +31,31 @@ int main(int argc, char* argv[])
 #endif
 
 #if 0
-	int a;
-	auto err = (input("%d") % a).get_error();
-	if(err == input::error::none) {
-		std::cout << "Input: " << a << std::endl;
-	} else {
-		std::cout << "Input error: " << static_cast<int>(err) << std::endl;
+	{  // test 3
+		int a;
+		auto err = (input("%d") % a).get_error();
+		if(err == input::error::none) {
+			std::cout << "Input: " << a << std::endl;
+		} else {
+			std::cout << "Input error: " << static_cast<int>(err) << std::endl;
+		}
 	}
 #endif
 
 #if 1
-	int a = 0;
-	uint32_t b = 0;
+	{  // test 4
+		int a = 0;
+		uint32_t b = 0;
 
-	static const char* inp = { "-99 100" };
-	auto n = (input("%d[, ]%x", inp) % a % b).num();
-	std::cout << "Num: " << n << std::endl;
-	std::cout << "1ST: " << a << ", 2ND: " << b << std::endl;
+		static const char* inp = { "-99 100" };
+		auto n = (input("%d[, ]%x", inp) % a % b).num();
+		std::cout << "Num: " << n << std::endl;
+		std::cout << "1ST: " << a << ", 2ND: " << b << std::endl;
+	}
 #endif
 
 #if 1
-	{
+	{  // test 5
 		int x = 0;
 		int y = 0;
 		int z = 0;
@@ -67,17 +71,38 @@ int main(int argc, char* argv[])
 
 
 #if 0
-	float a;
-	int b;
-	uint32_t c;
-	double d;
+	{  // test 6
+		float a;
+		int b;
+		uint32_t c;
+		double d;
 
-	static const char* inp = { "101.945,-76,7ff,1.567" };
-	auto n = (input("%f,%d,%x,%f", inp) % a % b % c % d).num();
-	std::cout << "Num: " << n << std::endl;
-	std::cout << "1ST: " << a << std::endl;
-	std::cout << "2ND: " << b << std::endl;
-	std::cout << "3RD: " << c << std::endl;
-	std::cout << "4TH: " << d << std::endl;
+		static const char* inp = { "101.945,-76,7ff,1.567" };
+		auto n = (input("%f,%d,%x,%f", inp) % a % b % c % d).num();
+		std::cout << std::endl;
+		std::cout << "Num: " << n << std::endl;
+		std::cout << "1ST: " << a << std::endl;
+		std::cout << "2ND: " << b << std::endl;
+		std::cout << "3RD: " << c << std::endl;
+		std::cout << "4TH: " << d << std::endl;
+	}
+#endif
+
+#if 1
+	{  // test 7
+		static const char* inp = { " 100 250 300" };
+		int a;
+		int b;
+		int c;
+		auto n = (input("%4d%4d%4d", inp) % a % b % c).num();
+		std::cout << std::endl;
+		if(n == 3 && a == 100 && b == 250 && c == 300) {
+			std::cout << "Test case 7: OK!\n";
+		}
+		std::cout << "Num: " << n << std::endl;
+		std::cout << "1ST: " << a << std::endl;
+		std::cout << "2ND: " << b << std::endl;
+		std::cout << "3RD: " << c << std::endl;
+	}
 #endif
 }
