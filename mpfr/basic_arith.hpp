ae4e9877f97460ffda78d0b4582db94eea2733f7
hirakuni45 <hira@rvf-rc45.net>
2020-11-24 17:50:13 +0900

    Update: cleanup

---
+++ mpfr/basic_arith.hpp
@@ -0,0 +1,394 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	Arithmetic テンプレート @n
+			※テキストの数式を展開して、計算結果を得る。@n
+			NVAL には、Boost Multiprecision Library を利用する事を前提にしている。
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2015, 2020 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include <cstdint>
+#include "bitset.hpp"
+
+namespace utils {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief	Arithmetic クラス
+		@param[in]	NVAL	基本型
+		@param[in]	SYMBOL	変数クラス
+		@param[in]	FUNC	関数クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <class NVAL, class SYMBOL, class FUNC>
+	struct basic_arith {
+
+		static const uint32_t NUMBER_NUM = 50;  // 最大桁数
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief	エラー・タイプ
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class error : uint8_t {
+			fatal,				///< エラー
+			number_fatal,		///< 数字の変換に関するエラー
+			zero_divide,		///< ０除算エラー
+			binary_fatal,		///< ２進データの変換に関するエラー
+			octal_fatal,		///< ８進データの変換に関するエラー
+			hexdecimal_fatal,	///< １６進データの変換に関するエラー
+			num_fatal,			///< 数値の変換に関するエラー
+			symbol_fatal,		///< シンボルデータの変換に関するエラー
+			func_fatal,			///< 関数の変換に関するエラー
+		};
+
+		typedef bitset<uint16_t, error> error_t;
+
+	private:
+
+		SYMBOL&		symbol_;
+		FUNC&		func_;
+
+		const char*		tx_;
+		char			ch_;
+
+		error_t		error_;
+
+		NVAL		value_;
+
+
+		// スペース、TAB を取り除く
+		void skip_space_() noexcept
+		{
+			while(ch_ == ' ' || ch_ == '\t') {
+				ch_ = *tx_++;
+			}
+		}
+
+
+		// 関数内パラメーターの取得
+		bool param_(char* dst, uint32_t len)
+		{
+			if(ch_ != '(') return false;
+
+			while(ch_ != 0) {
+				--len;
+				if(len == 0) break;
+			}
+
+			return true;
+		}
+
+
+		NVAL number_()
+		{
+			bool minus = false;
+			char tmp[NUMBER_NUM];
+
+			skip_space_();
+
+			// 符号、反転の判定
+			if(ch_ == '-') {
+				minus = true;
+				ch_ = *tx_++;
+			} else if(ch_ == '+') {
+				ch_ = *tx_++;
+			}
+
+			skip_space_();
+
+			NVAL nval;
+			if(static_cast<uint8_t>(ch_) >= 0x80) {  // symbol?, func?
+				if(static_cast<uint8_t>(ch_) >= 0xC0) {  // func ?
+					auto fc = static_cast<typename FUNC::NAME>(ch_);
+					ch_ = *tx_++;
+					if(ch_ == '(') {
+						ch_ = *tx_++;
+						auto param = expression_();
+						if(ch_ == ')') {
+							ch_ = *tx_++;
+							if(!func_(fc, param, nval)) {
+								error_.set(error::func_fatal);
+							}
+						} else {
+							error_.set(error::fatal);
+						}
+					} else {
+						error_.set(error::func_fatal);
+					}
+				} else {  // to symbol
+					if(symbol_(static_cast<typename SYMBOL::NAME>(ch_), nval)) {
+						ch_ = *tx_++;	
+					} else {
+						error_.set(error::symbol_fatal);
+					}
+				}
+				return nval;
+			}
+
+			if(ch_ == '(') {
+				nval = factor_();
+			} else {
+				uint32_t idx = 0;
+				while(ch_ != 0) {
+					if(ch_ == '+') break;
+					else if(ch_ == '-') break;
+					else if(ch_ == '*') break;
+					else if(ch_ == '/') break;
+					else if(ch_ == ')') break;
+					else if((ch_ >= '0' && ch_ <= '9') || ch_=='.' || ch_=='e' || ch_=='E') {
+						tmp[idx] = ch_;
+						idx++;
+					} else {
+						error_.set(error::fatal);
+						break;
+					}
+					ch_ = *tx_++;
+				}
+				tmp[idx] = 0;
+				if(error_() == 0) {
+					nval.assign(tmp);
+				}
+			}
+
+			if(minus) { nval = -nval; }
+
+			return nval;
+		}
+
+
+		auto factor_()
+		{
+			NVAL v(0.0);
+			if(ch_ == '(') {
+				ch_ = *tx_++;
+				v = expression_();
+				if(ch_ == ')') {
+					ch_ = *tx_++;
+				} else {
+					error_.set(error::fatal);
+				}
+			} else {
+				v = number_();
+			}
+			return v;
+		}
+
+
+		NVAL term_() {
+			NVAL v = factor_();
+			NVAL tmp;
+			while(error_() == 0) {
+				switch(ch_) {
+				case ' ':
+				case '\t':
+					ch_ = *tx_++;
+					break;
+				case '*':
+					ch_ = *tx_++;
+					v *= factor_();
+					break;
+#if 0
+				case '%':
+					ch_ = *tx_++;
+					tmp = factor_();
+					if(tmp == 0) {
+						error_.set(error::zero_divide);
+						break;
+					}
+					v %= tmp;
+					break;
+#endif
+				case '/':
+					ch_ = *tx_++;
+					if(ch_ == '/') {
+						ch_ = *tx_++;
+						tmp = factor_();
+						if(tmp == 0) {
+							error_.set(error::zero_divide);
+							break;
+						}
+					} else {
+						tmp = factor_();
+						if(tmp == 0) {
+							error_.set(error::zero_divide);
+							break;
+						}
+						v /= tmp;
+					}
+					break;
+#if 0
+				case '<':
+					ch_ = *tx_++;
+					if(ch_ == '<') {
+						ch_ = *tx_++;
+						v <<= factor_();
+					} else {
+						error_.set(error::fatal);
+					}
+					break;
+				case '>':
+					ch_ = *tx_++;
+					if(ch_ == '>') {
+						ch_ = *tx_++;
+						v <<= factor_();
+					} else {
+						error_.set(error::fatal);
+					}
+					break;
+#endif
+				default:
+					return v;
+					break;
+				}
+			}
+			return v;
+		}
+
+
+		NVAL expression_() {
+			NVAL v = term_();
+			while(error_() == 0) {
+				switch(ch_) {
+				case ' ':
+				case '\t':
+					ch_ = *tx_++;
+					break;
+				case '+':
+					ch_ = *tx_++;
+					v += term_();
+					break;
+				case '-':
+					ch_ = *tx_++;
+					v -= term_();
+					break;
+#if 0
+				case '&':
+					ch_ = *tx_++;
+					v &= term_();
+					break;
+				case '^':
+					ch_ = *tx_++;
+					v ^= term_();
+					break;
+				case '|':
+					ch_ = *tx_++;
+					v |= term_();
+					break;
+#endif
+				default:
+					return v;
+					break;
+				}
+			}
+			return v;
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	コンストラクター
+			@param[in]	func	関数クラス
+		*/
+		//-----------------------------------------------------------------//
+		basic_arith(SYMBOL& symbol, FUNC& func) noexcept : symbol_(symbol), func_(func),
+			tx_(nullptr), ch_(0), error_(), value_()
+		{ }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	解析を開始
+			@param[in]	text	解析テキスト
+			@return	文法にエラーがあった場合、「false」
+		*/
+		//-----------------------------------------------------------------//
+		bool analize(const char* text)
+		{
+			if(text == nullptr) {
+				error_.set(error::fatal);
+				return false;
+			}
+			tx_ = text;
+
+			error_.clear();
+
+			ch_ = *tx_++;
+			if(ch_ != 0) {
+				value_ = expression_();
+			} else {
+				error_.set(error::fatal);
+			}
+
+			if(error_() != 0) {
+				return false;
+			} else if(ch_ != 0) {
+				error_.set(error::fatal);
+				return false;
+			}
+			return true;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	エラーを受け取る
+			@return エラー
+		*/
+		//-----------------------------------------------------------------//
+		const error_t& get_error() const { return error_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	エラーメッセージを取得
+			@return エラーメッセージ
+		*/
+		//-----------------------------------------------------------------//
+		template <class STR>
+		STR get_error() const {
+			STR str;
+			return str;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	埋め込まれた、シンボル、関数名を展開
+			@param[in]	in		入力文字列
+			@param[out]	out		展開文字列
+			@param[in]	len		展開文字列サイズ
+			@return	成功なら「true」
+		*/
+		//-----------------------------------------------------------------//
+		bool parse(const char* in, char* out, uint32_t len) const
+		{
+			char ch;
+			while(len > 1 && (ch = *in++) != 0) {
+				uint8_t n = static_cast<uint8_t>(ch);
+				if(n < 0x80) {
+					*out++ = ch;
+					len--;
+				} else if(n < 0xc0) {  // シンボル
+
+				} else {  // 関数
+
+				}
+			}
+			*out = 0;
+			return len > 1;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	() で結果を取得
+			@return	結果
+		*/
+		//-----------------------------------------------------------------//
+		NVAL operator() () const { return value_; }
+	};
+}
--- mpfr/main.cpp
+++ mpfr/main.cpp
@@ -1,53 +1,77 @@
-#include <iostream>
-#include <boost/format.hpp>
-#include <limits>
-
-#include <mpfr.h>
-
 #include "format.hpp"
+#include "mpfr.hpp"
+
+#include "basic_arith.hpp"
+#include "calc_symbol.hpp"
+#include "calc_func.hpp"
 
 namespace {
 
+#if 0
 	void test_mpfr_()
 	{
 		mpfr_t a;
 		mpfr_t b;
 		mpfr_t c;
-		mpfr_t x;
-		mpfr_t y;
 
-		mpfr_init2 (c, 50);
-		mpfr_set_d (c, 2.0, MPFR_RNDD);
+		mpfr_init2 (a, 100);
+		mpfr_init2 (b, 100);
 
-		mpfr_init2 (y, 50);
-		mpfr_set_d (y, -0, MPFR_RNDD);
-		mpfr_init2 (x, 50);
-		mpfr_set_d (x, +0, MPFR_RNDD);
+		mpfr_init2 (c, 100);
+		mpfr_set_d (c, 2.0, MPFR_RNDN);
 
-		mpfr_init2 (a, 50);
-		mpfr_init2 (b, 50);
+		mpfr_sqrt(a, c, MPFR_RNDN);
 
-		mpfr_sqrt(a, c, MPFR_RNDD);
+		mpfr_const_pi(b, MPFR_RNDN);
 
-		mpfr_const_pi(b, MPFR_RNDD);
-
-		mpfr_printf("%.50RNf\n", a);
-		mpfr_printf("%.50RNf\n", b);
+		mpfr_printf("%.95RNf\n", a);
+		mpfr_printf("%.95RNf\n", b);
 
 		mpfr_clear (c);
-		mpfr_clear (x);
-		mpfr_clear (y);
-		mpfr_clear (a);
 		mpfr_clear (b);
+		mpfr_clear (a);
 	}
+#endif
 
+	typedef mpfr::value<50> NVAL;
+
+	typedef utils::calc_def CDEF;
+
+	typedef utils::calc_symbol<NVAL> SYMBOL;
+	SYMBOL	symbol_;
+
+	typedef utils::calc_func<NVAL> FUNC;
+	FUNC	func_;
+
+	typedef utils::basic_arith<NVAL, SYMBOL, FUNC> ARITH;
+	ARITH	arith_(symbol_, func_);
 }
 
 int main(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
-	using namespace utils;
+#if 0
+	VAL a(2.0);
+	VAL b(4.0);
 
-	test_mpfr_();
+	a = a / b;
+
+	char tmp[100];
+	a(tmp, sizeof(tmp));
+	utils::format("%s\n") % tmp;
+#endif
+	{
+		char tmp[100];
+//		utils::sformat("%c(2)", tmp, sizeof(tmp)) % static_cast<char>(CDEF::NAME::SQRT);
+		utils::sformat("%c(45)", tmp, sizeof(tmp)) % static_cast<char>(CDEF::NAME::SIN);
+		arith_.analize(tmp);
+	}
+
+	{
+		auto ans = arith_();
+		char tmp[200];
+		ans(tmp, sizeof(tmp));
+		utils::format("%s\n") % tmp;
+	}
 }
